60c8c77a11ed48f76e2585e92437cbe8fe55ba18
Cno <dsyourshy@qq.com>
2025-05-10 23:48:34 +0800

    chore: 更新文案

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center h-full">
     <div class="mb-15vh flex flex-col gap-16px items-center w-60%">
-      <h1 class="text-4xl font-bold">Get Some Packages</h1>
+      <h1 class="text-4xl font-bold">Get Some Real Packages</h1>
       <IconField class="w-full">
         <InputIcon class="pi pi-search" />
         <InputText
